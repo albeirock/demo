2959923f1e7533024482cdad0cf5c80eb9105bdc
albeirock <albeirocaucali@gmail.com>
2021-05-05 20:39:24 -0500

    version 1 1. Diseño responsivo 2. página de inicio 3. botón inicio

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
-<?php
-echo "hola que hace";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>PREICFES COCO</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    
+</head>
+<body>
 
-<?php
-echo "hola que hace";
-?>
+<div class="box_sesion">
+    <br>
+    <br> 
+      
+    <h1>Simulacro prueba SABER 11</h1>
+    <br>
+    <br> 
+    <a href="login.php"><button type="button" class="btn btn-primary btn-lg">INICIAR SESION</button></a>
+    
+
+</div>
+
+
+
+
+   
+
+</body>
+</html>
